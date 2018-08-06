74b1105f78c9ab3d3fb83d9218845e4b221cd6d8
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-06 15:43:36 +0100

    Finish exercises

--- specs/sports_team_specs.rb
+++ specs/sports_team_specs.rb
@@ -25,4 +25,19 @@ class TestSportsTeam < Minitest::Test
     @sports_team.coach = "Juan"
     assert_equal("Juan", @sports_team.coach)
   end
+
+  def test_add_player
+    new_team = @sports_team.add_player("Jose")
+    assert_equal(["Jesse", "Alvaro", "James", "Sharon", "Jose"], new_team)
+  end
+
+  def test_find_player?
+    assert_equal(true, @sports_team.find_player?("Jesse"))
+  end
+
+  def test_check_points_for_win
+    @sports_team.win_points("win")
+    assert_equal(1, @sports_team.points)
+  end
+
 end
--- sports_team.rb
+++ sports_team.rb
@@ -1,12 +1,29 @@
 class SportsTeam
 
-  attr_reader(:team_name, :players, :coach)
+  attr_reader(:team_name, :players, :coach, :points)
   attr_writer(:coach)
 
   def initialize(team_name, players, coach)
     @team_name = team_name
     @players = players
     @coach = coach
+    @points = 0
+  end
+
+  def add_player(name)
+    @players.push(name)
+  end
+
+  def find_player?(name)
+    for player in @players
+      return true if name == player
+    end
+
+    return false
+  end
+
+  def win_points(result)
+    return @points += 1 if result == "win"
   end
 
   # def change_coach(new_coach)
