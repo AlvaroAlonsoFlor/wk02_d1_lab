8f1712aed0e7d596734db55009936371fd0ca744
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-06 15:11:35 +0100

    All methods explicitly defined

--- specs/sports_team_specs.rb
+++ specs/sports_team_specs.rb
@@ -1,3 +1,23 @@
 require("minitest/autorun")
 require("minitest/rg")
-require_relative("../bank_account.rb")
+require_relative("../sports_team.rb")
+
+class TestSportsTeam < Minitest::Test
+
+  def setup
+    @sports_team = SportsTeam.new("Barx", 23, "Emilio")
+  end
+
+  def test_get_team
+
+    assert_equal("Barx", @sports_team.team)
+  end
+
+  def test_get_players
+    assert_equal(23, @sports_team.players)
+  end
+
+  def test_get_coach
+    assert_equal("Emilio", @sports_team.coach)
+  end
+end
